29547881ca6515f12b37ef791d14890d2a247fd3
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-04-12 22:05:08 +0200

    implement basic example

--- src/BasicExample.elm
+++ src/BasicExample.elm
@@ -1,19 +1,72 @@
-module BasicExample exposing (update)
+module BasicExample exposing (main)
 
--- This is what using Random.Name
--- would look like in parts of a
--- TEA web app, with the builtin
--- random name space
+import Browser
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html)
+import Html.Attributes
+import Random
+import Random.Name exposing (randomName)
+
+
+type alias Model =
+    { name : String
+    , seed : Random.Seed
+    }
+
+
+type Msg
+    = RandomName
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \model -> Sub.none
+        }
+
+
+init flags =
+    ( { name = "initial value"
+      , seed = Random.initialSeed 239846
+      }
+    , Cmd.none
+    )
 
 
 update msg model =
+    let
+        ( name, newSeed ) =
+            Random.step randomName model.seed
+    in
     case msg of
-        MakeNewName ->
-            ( model
-            , Random.generate NewName randomName
-            )
+        RandomName ->
+            ( { name = name, seed = newSeed }, Cmd.none )
 
-        NewName newName ->
-            ( { model | instanceName = toString newName }
-            , Cmd.none
-            )
+
+view { name } =
+    layout [ width fill, height fill ] <|
+        column
+            [ centerX, centerY, spacing 20, width (px 250) ]
+            [ el [ centerX ] (text name)
+            , Input.button
+                [ Background.color (rgb255 238 238 238)
+                , padding 10
+                , centerX
+                , focused [ Background.color (rgb255 238 0 238) ]
+                ]
+                { onPress = Just RandomName
+                , label =
+                    el
+                        [ Font.center
+                        , width (px 180)
+                        ]
+                        (text "Random name")
+                }
+            ]
